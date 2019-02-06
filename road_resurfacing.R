5b1a314861946279c98a11cf09048784c8680e19
seanolondon <Sean.ODonnell@london.gov.uk>
2019-02-06 13:04:32 +0000

    reading in lines (shp) and resurfacing data

--- road_resurfacing.R
+++ road_resurfacing.R
@@ -8,5 +8,58 @@
 library(dplyr)
 library(magrittr)
 library(tidyr)
+library(sf)
+library(readxl)
+library(rmarkdown)
+library(purrr)
+library(ggplot2)
+library(ggmap)
 
-#hello world
+#Get the file
+setwd("R:/K/Projects/Development/Planning/London_Infrastructure_Plan_2050/scripts/road_resurfacing")
+
+source("westminster_roads.R")
+
+westminster_road <- read_xlsx(westminster_road_drive_location)
+
+#merge all roads as the base dataset
+
+london_roads_folder <- "Q:/Teams/GIS&I/GIS/Processing/OrdnanceSurvey/OS Highways Network/output/London-wide streets/"
+
+files <- dir(path = london_roads_folder, pattern = ".shp") 
+files <- files[!grepl(pattern = "lock", files)]
+
+
+london_roads <- list()
+
+for(l in 1:length(files)){
+  nested_sfs <- st_read(dsn = paste0(london_roads_folder,files[l]), options = "ENCODING=UTF-8")
+  name <- paste('item:',l,sep='')
+  tmp <- list(table = nested_sfs)
+  london_roads[[name]] <- tmp
+}
+
+london_roads <- do.call("rbind", flatten(london_roads)) %>% 
+  unique() %>%
+  select(USRN,
+         beginLifes,
+         identifier,
+         validFrom,
+         operatio06,
+         operatio08,
+         responsi00,
+         designat03,
+         designat06,
+         administ01,
+         town1,
+         gssCode1)
+
+subset <- head(london_roads, n = 1000)
+
+####################################
+
+bbox <- st_bbox(subset)
+
+display_map <- get_map(location = bbox, maptype = "toner", source = "stamen")
+
+ggmap(display_map) + geom_line(data = subset, mapping = aes(x = lon, y = lat), color = "red")
