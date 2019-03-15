5fc5b85708a127fdb97469cd2f71dd77caf2c1d3
seanolondon <Sean.ODonnell@london.gov.uk>
2019-03-15 16:13:54 +0000

    a redo of road resurfaciing
    redoing the code to use more up to date roads layer, plus a nodes layer. The idea is to use start nodes and end nodes, currently building a table of all the intersections at a node to match names

--- road_resurfacing.R
+++ road_resurfacing.R
@@ -11,20 +11,26 @@
 library(dplyr)
 library(magrittr)
 library(tidyr)
-library(sf)
+library(sp)
 library(readxl)
 library(rmarkdown)
-library(purrr, lib = "M:/R/R-3.4.3")
-library(ggplot2, lib = "M:/R/R-3.4.3")
-library(ggmap, lib = "M:/R/R-3.4.3")
-library(leaflet, lib = "M:/R/R-3.4.3") 
+library(purrr)
+library(ggplot2)
+library(ggmap)
+library(leaflet)
+library(sf, lib = .libPaths()[2])
 library(lwgeom)
-
+#library(stplanr)
+library(osmar)
+#install.packages(c("sf", "lwgeom", "stplanr", "osmar", "stplanr","dplyr","magrittr","tidyr","sp","readxl","rmarkdown","purrr","ggplot2","ggmap","leaflet"), lib = .libPaths()[1], dependancies = TRUE)
+#install.packages("stplanr", lib = .libPaths()[1])
 
 #Get the file
 setwd("R:/K/Projects/Development/Planning/London_Infrastructure_Plan_2050/scripts/road_resurfacing")
 
 source("westminster_roads.R")
+
+st_erase = function(x, y) st_difference(x, st_union(st_combine(y)))
 
 westminster_council_table <- read_xlsx(westminster_road_drive_location, na = c("TBC", "n/a", "#VALUE!"))
 westminster_council_table$GSS_CODE<-"E09000033"
@@ -36,63 +42,34 @@ boroughs <- st_transform(boroughs, 27700)
 
 borough_table <- st_set_geometry(boroughs, NULL)
 
-
-
 #merge all roads as the base dataset
 
-london_roads_folder <- "Q:/Teams/GIS&I/GIS/Processing/OrdnanceSurvey/OS Highways Network/output/London-wide streets/"
+london_roads_folder <- "R:/K/Projects/Development/Planning/London_Infrastructure_Plan_2050/scripts/road_resurfacing/os_highways_2019.gpkg"
 
-files <- dir(path = london_roads_folder, pattern = ".shp") 
-files <- files[!grepl(pattern = "lock", files)]
+london_roads <- st_read(dsn = london_roads_folder, layer = "Network_RoadLink_N", crs = 27700)
 
+london_roads_nodes <- st_read(dsn = london_roads_folder, layer = "Network_Network_ND_Junctions", crs = 27700)
 
-london_roads <- list()
-
-for(l in 1:length(files)){
-  nested_sfs <- st_read(dsn = paste0(london_roads_folder,files[l]), options = "ENCODING=UTF-8") %>%
-    st_transform(27700)
-  name <- paste('item:',l,sep='')
-  tmp <- list(table = nested_sfs)
-  london_roads[[name]] <- tmp
-}
-
-london_roads <- do.call("rbind", purrr::flatten(london_roads)) %>%
-  select(USRN,
-         beginLifes,
-         identifier,
-         validFrom,
-         operatio06,
-         operatio08,
-         responsi00,
-         designat03,
-         designat06,
-         administ01,
-         town1,
-         gssCode1) %>%
-  filter(!is.na(designat03)) %>%
-  unique() %>%
-  st_transform(27700)
-
-london_roads <- st_transform(london_roads, 27700)
-
-#test#
-#subset <- head(london_roads, n = 1000) %>%
-#  st_transform(4326)
-######
+#But if node hits street with same name dont duplicate
+#stplanr
 
 
-london_roads_boroughs <- left_join(london_roads, borough_table, by = c("gssCode1" = "GSS_CODE")) 
+#london_roads_boroughs <- left_join(london_roads, borough_table, by = c("gssCode1" = "GSS_CODE")) 
+borough_of_interest <- boroughs %>%
+  filter(GSS_CODE == "E09000033")
 
 #filter to westminster roads#
 #THERE ARE DUPLICATE ROAD NAMES, WHICH ARE IN FACT DIFFERENT ROADS. THEY HAVE DIFFERENT AND DISCTINCT SPATIAL LOCATIONS#
-london_roads_westminster <- london_roads_boroughs %>%
-  filter(gssCode1 == "E09000033") %>%
+london_roads_westminster <- london_roads %>%
+  st_intersection(borough_of_interest)%>%
   rename_all(tolower) %>%
-  mutate(road_name_lwr = tolower(designat03)) %>%
+  mutate(road_name_lwr = tolower(roadname1)) %>%
   mutate(road_name_clean = stringr::str_replace_all(string=road_name_lwr, pattern=" ", repl="")) %>%
   mutate(road_name_clean = stringr::str_trim(road_name_clean)) %>%
   mutate(road_name_clean = gsub(pattern = "[^a-zA-Z0-9]", replacement = "", road_name_clean))
 
+london_roads_nodes_westminster <- london_roads_nodes %>%
+  st_intersection(borough_of_interest)
 
 #reformat the council provided road resurfacing table
 colnames(westminster_council_table) <- gsub(pattern = "[^a-zA-Z0-9]", replacement = "", colnames(westminster_council_table))
@@ -106,19 +83,26 @@ westminster_council_table <- westminster_council_table %>%
   mutate(road_name_clean = stringr::str_trim(road_name_clean)) %>%
   mutate(road_name_clean = gsub(pattern = "[^a-zA-Z0-9]", replacement = "", road_name_clean))
 
-#testing the start and finish clumns#
+#flag if a square
 westminster_council_table_intersections <- westminster_council_table$roadname %>%
   stringr::str_detect("Square|square") %>%
   cbind(westminster_council_table) %>%
   mutate(square_1 = as.numeric(.)) %>%
   select(-.)  
 
-
+#flag if a junction/roundabout
 westminster_council_table_intersections <- westminster_council_table_intersections$locationextents %>%
-  stringr::str_detect("Junction|junction") %>%
+  stringr::str_detect("Junction|junction|Roundabout|roundabout") %>%
   cbind(westminster_council_table_intersections) %>%
   mutate(junction_1 = as.numeric(.)) %>%
   select(-.) 
+
+#flag if to the end
+westminster_council_table_intersections <- westminster_council_table_intersections$locationextents %>%
+  stringr::str_detect("to End|to end") %>%
+  cbind(westminster_council_table_intersections) %>%
+  mutate(end_1 = as.numeric(.)) %>%
+  select(-.)
 
 westminster_council_table_intersections <- westminster_council_table_intersections$locationextents %>%
   stringr::str_split_fixed(pattern = " to |&", n = Inf) %>%
@@ -141,6 +125,33 @@ westminster_council_table_intersections <- westminster_council_table_intersectio
   mutate(intersection_3_clean = gsub(pattern = "[^a-zA-Z0-9]", replacement = "", intersection_3_clean))
 
 
+london_roads_westminster <- london_roads_westminster %>%
+  select(roadname1) %>%
+  
+
+london_roads_nodes_westminster <- london_roads_nodes_westminster %>%
+  st_join(london_roads_westminster) %>%
+  select(OBJECTID, roadname1) %>%
+  group_by(OBJECTID) %>% 
+  mutate(touching = row_number()) %>%
+  spread(touching, roadname1, sep = "_")
+  
+london_roads_nodes_westminster_df_clean <- st_set_geometry(london_roads_nodes_westminster, NULL) %>%
+  lapply(tolower) %>%
+  as_tibble() %>%
+  lapply(FUN = function(x) gsub(" ", "", x)) %>%
+  as_tibble() %>%
+  lapply(FUN = function(x) gsub("[^a-zA-Z0-9]", "", x)) %>%
+  as_tibble()
+
+
+agrep(pattern, x, max.distance = 0.1, costs = NULL,
+      ignore.case = FALSE, value = FALSE, fixed = TRUE,
+      useBytes = FALSE)
+
+
+
+
 #get ALL roads with a match - could be duplicated if roads with same name#
 roads_with_resurfacing <- full_join(london_roads_westminster, westminster_council_table_intersections, by = "road_name_clean") %>%
   filter(!is.na(roadname))
@@ -159,117 +170,32 @@ london_roads_westminster <- sapply(st_intersects(london_roads_westminster, borou
 
 #roads_with_resurfacing[4,]
 #filter out squares and junctions! 
-roads_with_resurfacing <- roads_with_resurfacing %>%
+roads_with_resurfacing_simple <- roads_with_resurfacing %>%
   filter(square_1 < 1) %>%
-  filter(junction_1 < 1)
+  filter(junction_1 < 1) %>%
+  filter(end_1 < 1)
 
 roads_with_resurfacing_clipped <- list()
 
-#create the roads_with_resurfacing_clipped list through a loop of the dataframe
-for(i in 1:nrow(roads_with_resurfacing)) {
-
-resurfacing_road_unclipped <- roads_with_resurfacing[i,]
-
-resurfacing_road_unclipped <- st_cast(resurfacing_road_unclipped, "MULTILINESTRING") 
-
-resurfacing_road_unclipped_df <- st_set_geometry(resurfacing_road_unclipped, NULL)
-
-resurfaced_road <- resurfacing_road_unclipped_df$road_name_clean[1]
-
-#150 METERS IS ARBITRARY! it accounts for the problem with mislabelled streets or places where streets change
-touching_resurfaced_roads <- sapply(st_is_within_distance(london_roads_westminster, resurfacing_road_unclipped, dist = 150),function(x){length(x)!=0}) %>%
-  subset(london_roads_westminster, subset = .) %>%
-  mutate(base_road_name = resurfaced_road)
-
-#filter out the resurfaced road spatially
-clipping_resurfaced_roads <- sapply(st_equals(touching_resurfaced_roads, resurfacing_road_unclipped),function(x){length(x)==0}) %>%
-  subset(touching_resurfaced_roads, subset = .) %>%
-  left_join(resurfacing_road_unclipped_df, by = c("base_road_name" = "road_name_clean")) %>%
-  filter(road_name_clean == intersection_1_clean | road_name_clean == intersection_2_clean | road_name_clean == intersection_3_clean) %>%
-  st_cast("MULTILINESTRING")
-
-#check if al lthe line pieces are touching
-clipping_resurfaced_roads <- clipping_resurfaced_roads[as.vector(st_intersects(clipping_resurfaced_roads, resurfacing_road_unclipped, sparse = FALSE)),]
-
-st_erase = function(x, y) st_difference(x, st_union(st_combine(y)))
-
-#if nointersect, this oculd be adifference between maps e.g. OS and Google maps
-#extend the line to intersect, snap, then erase the overlapping area
-if(st_intersects(resurfacing_road_unclipped, clipping_resurfaced_roads, sparse = FALSE) == FALSE){
-  print("no intersect")
-  clipping_resurfaced_roads <- st_snap(clipping_resurfaced_roads, resurfacing_road_unclipped, tolerance = 150) %>%
-    st_erase(resurfacing_road_unclipped)
-} else {
-  print("ALL GOOD")
-}
-
-#####nee to get the intersect points  
-
-#create a sf dataframe of all the intersection points between the clipping roads and the resurfaced roads
-#in the even of multilines, these will create multi points, these need to be changed to individual points 
-intersection_multipoints <- st_intersection(resurfacing_road_unclipped, clipping_resurfaced_roads) 
-
-intersection_points <- list()
-
-#unpack the points and multipoints into a list made up of point sf dataframes (removing multipoints)
-for(k in 1:nrow(intersection_multipoints)) {
-  intersection_points[[k]] <- st_cast(intersection_multipoints[k,], "POINT")
-}
-
-#unpack a list of sf dataframes into a single dataframe of points
-intersection_points <- do.call(rbind, intersection_points)
-
-#create an empty list of resurfaced road to put in 'pieces'
-split_resurfaced_road <- list()
-
-#split the resurfaced road into pieces from the intersection points, keep the line segments in a list
-for(n in 1:nrow(intersection_points)) {
-  split_resurfaced_road[[n]] <- lwgeom::st_split(st_union(resurfacing_road_unclipped$geometry), intersection_points[n,]$geometry) %>%
-    st_collection_extract("LINESTRING") #%>% 
-    #st_cast("LINESTRING")
-}
-
-#the lines are sfc objects in a list, turn them into sf objects but keep in a list
-split_resurfaced_road <- purrr::map(split_resurfaced_road, st_sf)
-
-#change the segments into a dataframe, these are all the segments possible
-split_resurfaced_road <- do.call(rbind, split_resurfaced_road)
-
-
-#need a logic test for which segment to select
-#intersects both and shorest distance
-split_resurfaced_road_shortest <- split_resurfaced_road %>%
-  mutate(intersection_counts = lengths(st_intersects(split_resurfaced_road, clipping_resurfaced_roads))) %>%
-  mutate(road_segment_length = st_length(split_resurfaced_road)) %>%
-  filter(intersection_counts > 1) %>%
-  arrange(road_segment_length) %>%
-  filter(row_number()==1)
-
-roads_with_resurfacing_clipped[[i]] <- split_resurfaced_road_shortest
-
-}
-
-roads_with_resurfacing_clipped <- purrr::map(roads_with_resurfacing_clipped, st_sf)
-
-
-roads_with_resurfacing_clipped <- do.call(rbind, roads_with_resurfacing_clipped1)
-
-#TESTFUZZYMATCH#
-#fuzzy_inner_join(y, by = c("string" = "seed"), match_fun = str_detect)
-
-#FILTER JOIN BY ROADNAME MATCH#
-#CLIP BY TOUCHING/INTERSECTING JUNCTIONS#
+stuck here!!!
   
-#agrep
-
-
+for(i in 1:nrow(roads_with_resurfacing_simple)) {
+  
+  resurfacing_road_unclipped <- roads_with_resurfacing_simple[1,] %>%
+    st_cast("MULTILINESTRING") 
+  
+  resurfacing_road_unclipped_df <- st_set_geometry(resurfacing_road_unclipped, NULL)
+  
+  print(resurfacing_road_unclipped_df$road_name_clean[1])
+  
+}
 
 #plotting only#
 #put into wgs for plotting
 intersecting_roads <- clipping_resurfaced_roads %>%
   st_transform(4326)  
-  
-roads_by_borough_plot <- road_to_clip %>%
+
+roads_by_borough_plot <- roads_with_resurfacing %>%
   st_transform(4326)
 
 base_roads_by_borough_plot <- london_roads_westminster %>%
@@ -278,11 +204,11 @@ base_roads_by_borough_plot <- london_roads_westminster %>%
 boroughs_plot <- boroughs %>%
   st_transform(4326)
 
-road_with_resurfaceing_plot <- roads_with_resurfacing %>%
-  st_transform(4326)
+#road_with_resurfaceing_plot <- resurfacing_road_unclipped %>%
+#  st_transform(4326)
 
-split_resurfaced_road_plot <- roads_with_resurfacing_clipped %>%
-  st_transform(4326)
+#split_resurfaced_road_plot <- roads_with_resurfacing_clipped %>%
+#  st_transform(4326)
 
 leaflet() %>%
   addMapPane(name = 'base', zIndex = 1) %>%
@@ -297,22 +223,19 @@ leaflet() %>%
               opacity = 0.2,
               options = leafletOptions(pane='themes')) %>%
   addPolylines(data = base_roads_by_borough_plot,
-              color = "green",
-              weight = 3,
-              opacity = 0.2,
-              options = leafletOptions(pane='themes')) %>%
-  addPolylines(data = road_with_resurfaceing_plot,
-             color = "red",
-             weight = 3,
-             options = leafletOptions(pane='themes')) %>%
-  addPolylines(data = intersecting_roads,
-               color = "yellow",
-               weight = 4,
-              options = leafletOptions(pane='themes')) %>%
-  addPolylines(data = split_resurfaced_road_plot,
-            color = "orange",
-            weight = 5,
-            options = leafletOptions(pane='themes'))
-  
-
-
+               color = "green",
+               weight = 3,
+               opacity = 0.2,
+               options = leafletOptions(pane='themes')) %>%
+  addPolylines(data = roads_by_borough_plot,
+               color = "red",
+               weight = 3,
+               options = leafletOptions(pane='themes')) #%>%
+#addPolylines(data = intersecting_roads,
+#            color = "yellow",
+#           weight = 4,
+#         options = leafletOptions(pane='themes')) %>%
+#addPolylines(data = split_resurfaced_road_plot,
+#         color = "orange",
+#        weight = 5,
+#       options = leafletOptions(pane='themes'))
